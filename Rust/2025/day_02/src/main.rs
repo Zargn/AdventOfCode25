a618d24b07b756b94fc8bb443cb80a80857be733
Oskar Anderson <oskaranderson99@gmail.com>
2025-12-03 10:34:13 +0100

    Rust: 2025/day_02: First almost finished version.

--- Rust/2025/day_02/src/main.rs
+++ Rust/2025/day_02/src/main.rs
@@ -23,15 +23,56 @@ Part Two:
 
 */
 
-fn load_data(path: &str) -> Result<(), Box<dyn Error>> {
-    let lines = reader::get_lines(path)?;
+struct IDRange {
+    lower: u32,
+    upper: u32,
+}
 
-    todo!();
+impl IDRange {
+    fn parse(data_string: &str) -> Result<IDRange, Box<dyn Error>> {
+        let mut parts = data_string.split('-');
+        let lower = parts.next();
+        let upper = parts.next();
+        let (Some(lower), Some(upper)) = (lower, upper) else {
+            return Err(format!(
+                "Could not parse IDRange from data string: [{}]!",
+                data_string
+            )
+            .into());
+        };
+        Ok(IDRange {
+            lower: lower.parse()?,
+            upper: upper.parse()?,
+        })
+    }
+
+    fn invalid_id_sum(&self) -> u64 {
+        let mut id_sum = 0;
+        for id in self.lower..self.upper {
+            let id_string = id.to_string();
+            let id_string_len = id_string.chars().count();
+            if (id_string_len % 2) != 0 {
+                continue;
+            }
+            if id_string[0..id_string_len / 2] == id_string[id_string_len / 2..] {
+                id_sum += id as u64;
+            }
+        }
+        id_sum
+    }
 }
 
 fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-    let data = load_data(data_path)?;
-    todo!();
+    let line = reader::get_lines(data_path)?
+        .next()
+        .expect("The data files for this challenge always only contain 1 line.");
+    let mut id_sum = 0;
+    for data_string in line.split(',') {
+        let id_range = IDRange::parse(data_string)?;
+        id_sum += id_range.invalid_id_sum();
+    }
+
+    Ok(id_sum)
 }
 
 fn main() {
