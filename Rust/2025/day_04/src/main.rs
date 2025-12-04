fbfa6a7684d8164951a088e482aba831e65fa791
Oskar Anderson <oskaranderson99@gmail.com>
2025-12-04 14:19:48 +0100

    Rust: 2025/day_04: Completed Part One

--- Rust/2025/day_04/src/main.rs
+++ Rust/2025/day_04/src/main.rs
@@ -12,8 +12,12 @@ We need to figure out which occupied tiles have at most 4 other occupied tiles i
 
 We need to build a two dimensional array of tiles first.
 Then iterate thorugh all tiles.
-For each tile we chack the surrounding 8 for other occupied tiles and count them.
+For each occupied tile we chack the surrounding 8 for other occupied tiles and count them.
 If there are at most 3 other occupied tiles then add 1 to a counter.
+
+To check the surrounding tiles for occupied tiles we could code it to only check those tiles ignoring the
+current one. But it might be better to check all 9 tiles in a square and just increase the allowed tiles
+by one. (Since the center tile would always be occupied.)
 
 Once all tiles have been visited then return the count value.
 
@@ -29,15 +33,94 @@ Part Two:
 
 */
 
-fn load_data(path: &str) -> Result<(), Box<dyn Error>> {
-    let lines = reader::get_lines(path)?;
+#[derive(Clone, Copy)]
+enum Tile {
+    Empty,
+    Occupied,
+}
 
-    todo!();
+impl Tile {
+    fn parse(char: char) -> Result<Tile, Box<dyn Error>> {
+        match char {
+            '.' => Ok(Tile::Empty),
+            '@' => Ok(Tile::Occupied),
+            _ => Err(format!("Tile parse error! Invalid character [{char}]!").into()),
+        }
+    }
+}
+
+const MAPSIZE: usize = 136 + 2; // 136 is the size of the largest data file.
+                                // Add 2 for a extra layer of empty tiles around the edges.
+
+struct Map {
+    grid: [[Tile; MAPSIZE]; MAPSIZE],
+    size_override: usize,
+}
+
+impl Map {
+    fn load_from_file(path: &str) -> Result<Map, Box<dyn Error>> {
+        let lines = reader::get_lines(path)?;
+        let mut grid = [[Tile::Empty; MAPSIZE]; MAPSIZE];
+        let mut size_override = 0;
+        for (y, line) in lines.enumerate() {
+            for (x, char) in line.chars().enumerate() {
+                grid[x + 1][y + 1] = Tile::parse(char)?;
+            }
+            size_override = y + 1;
+        }
+
+        Ok(Map {
+            grid,
+            size_override,
+        })
+    }
+
+    fn accessable_tiles(&self) -> u64 {
+        let mut accessable_tiles = 0;
+        for y in 1..self.size_override + 1 {
+            for x in 1..self.size_override + 1 {
+                if let Tile::Empty = self.grid[x][y] {
+                    continue;
+                }
+
+                if self.is_accessable(x, y) {
+                    accessable_tiles += 1;
+                }
+            }
+        }
+        accessable_tiles
+    }
+
+    fn is_accessable(&self, c_x: usize, c_y: usize) -> bool {
+        let mut occupied = 0;
+        for y in c_y - 1..=c_y + 1 {
+            for x in c_x - 1..=c_x + 1 {
+                if let Tile::Occupied = self.grid[x][y] {
+                    occupied += 1;
+                }
+            }
+        }
+        occupied <= 4
+    }
+
+    /// Debug code to print the map in the same format as the data file.
+    #[allow(dead_code)]
+    fn print(&self) {
+        for y in 1..self.size_override + 1 {
+            for x in 1..self.size_override + 1 {
+                match self.grid[x][y] {
+                    Tile::Empty => print!("."),
+                    Tile::Occupied => print!("@"),
+                }
+            }
+            println!();
+        }
+    }
 }
 
 fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-    let data = load_data(data_path)?;
-    todo!();
+    let map = Map::load_from_file(data_path)?;
+    Ok(map.accessable_tiles())
 }
 
 fn main() {
