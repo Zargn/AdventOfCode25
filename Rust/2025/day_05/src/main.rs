cbbb6ccaa71a1bbfe425a56a9acaba2da29aa26a
Oskar Anderson <oskaranderson99@gmail.com>
2025-12-05 11:36:03 +0100

    Rust: 2025/day_05: Completed Part One

--- Rust/2025/day_05/src/main.rs
+++ Rust/2025/day_05/src/main.rs
@@ -14,7 +14,7 @@ The quick and easy solution would be to simply load the ranges into a "range" st
 and upper value and has a method "in_range" which takes a value and returns true/false depending on if the
 value fits inside the range.
 
-Simply a if statement: if value < lower || value > upper
+Simply a if statement: if value >= lower && value <= upper
 Iterate through the list of ranges until the value fits in a range or all ranges have been checked.
 If the value fits in a range then add 1 to a counter.
 
@@ -26,15 +26,60 @@ Part Two:
 
 */
 
-fn load_data(path: &str) -> Result<(), Box<dyn Error>> {
-    let lines = reader::get_lines(path)?;
+struct IDRange {
+    upper: u64,
+    lower: u64,
+}
 
-    todo!();
+impl IDRange {
+    fn parse(data_string: &str) -> Result<IDRange, Box<dyn Error>> {
+        let mut parts = data_string.split('-');
+        let (Some(lower), Some(upper)) = (parts.next(), parts.next()) else {
+            return Err(format!(
+                "Could not parse IDRange from data string: [{}]!",
+                data_string
+            )
+            .into());
+        };
+        Ok(IDRange {
+            upper: upper.parse()?,
+            lower: lower.parse()?,
+        })
+    }
+
+    fn in_range(&self, value: &u64) -> bool {
+        self.lower <= *value && *value <= self.upper
+    }
 }
 
 fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-    let data = load_data(data_path)?;
-    todo!();
+    let mut lines = reader::get_lines(data_path)?;
+    let mut ranges: Vec<IDRange> = Vec::new();
+
+    loop {
+        let Some(line) = lines.next() else {
+            return Err("Data file ended before any values was reached!".into());
+        };
+        if line.is_empty() {
+            break;
+        }
+        ranges.push(IDRange::parse(&line)?);
+    }
+
+    let mut fresh_ingredients: u64 = 0;
+    // This iter starts where the above loop stopped reading at the empty space.
+    for line in lines {
+        let value: u64 = line.parse()?;
+        for range in &ranges {
+            if range.in_range(&value) {
+                //println!("[{}] is in range [{}-{}]", value, range.lower, range.upper);
+                fresh_ingredients += 1;
+                break;
+            }
+        }
+    }
+
+    Ok(fresh_ingredients)
 }
 
 fn main() {
