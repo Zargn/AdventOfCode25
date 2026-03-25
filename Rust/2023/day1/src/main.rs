efd24e17cdae78fd6484e122fa3ac63bd7e46583
Oskar Anderson <oskaranderson99@gmail.com>
2026-03-25 14:26:02 +0100

    Rust: 2023/day_1: Completed Part One

--- Rust/2023/day1/src/main.rs
+++ Rust/2023/day1/src/main.rs
@@ -8,7 +8,7 @@ mod tests;
 #[allow(dead_code)]
 pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 142;
 #[allow(dead_code)]
-pub const PART_ONE_EXPECTED_VALUE: u64 = 0;
+pub const PART_ONE_EXPECTED_VALUE: u64 = 55971;
 
 #[allow(dead_code)]
 pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
@@ -23,15 +23,39 @@ pub const PART_TWO_EXPECTED_VALUE: u64 = 0;
 Part One
 ##################################################################################################
 
+The task here is to simply grab the first and the last digit of each line and adding them
+together. The first digit has to be multiplied by 10. (If only one digit exists then it is both
+first and last.)
+Then add the resulting values of each line together to get the result.
 */
 mod part_one {
     use crate::reader;
     use std::error::Error;
 
-    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+    fn get_calibration_value(data_str: &str) -> Result<u64, Box<dyn Error>> {
+        // for each char in data_str filter out any that are not ascii digits, then parse a int
+        // from each digit and collect into a vec.
+        let values: Vec<u32> = data_str
+            .chars()
+            .filter(|c| c.is_ascii_digit())
+            .map(|c| c.to_digit(10).unwrap()) // c is always a digit so this will never panic.
+            .collect();
 
-        Err("NotImplemented: This problem has not been solved yet!".into())
+        if values.is_empty() {
+            Err("No values found!".into())
+        } else {
+            // values will always contain at least one element so this will never panic.
+            Ok(((values.first().unwrap() * 10) + values.last().unwrap()) as u64)
+        }
+    }
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let mut result_value = 0;
+        for line in reader::get_lines(data_path)? {
+            result_value += get_calibration_value(&line)?;
+        }
+
+        Ok(result_value)
     }
 }
 
@@ -75,7 +99,7 @@ fn main() {
             Err(err) => format!("FAILED with error:\n{}", err),
         }
     });
-    println!("\nPart One {}\n", {
+    println!("\nPart Two {}\n", {
         match benchmark!("calculate", { part_two::calculate("data.txt") }) {
             Ok(value) => format!("Result:\n{}", value),
             Err(err) => format!("FAILED with error:\n{}", err),
