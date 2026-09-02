ca3f433d30de2976a125c37c204d0ca9f4eac6e1
Oskar Anderson <oskaranderson99@gmail.com>
2026-09-02 13:24:51 +0200

    Rust: 2023/day_13: Completed Part One

--- Rust/2023/day_13/src/main.rs
+++ Rust/2023/day_13/src/main.rs
@@ -8,7 +8,7 @@ mod tests;
 #[allow(dead_code)]
 pub const PART_ONE_EXPECTED_TEST_VALUE: u64 = 405;
 #[allow(dead_code)]
-pub const PART_ONE_EXPECTED_VALUE: u64 = 0;
+pub const PART_ONE_EXPECTED_VALUE: u64 = 29846;
 
 #[allow(dead_code)]
 pub const PART_TWO_EXPECTED_TEST_VALUE: u64 = 0;
@@ -25,7 +25,7 @@ Part One
 
 2D array time.
 We have a number of maps that contain ash '.' and rocks '#'. Our goal is to find a mirror line.
-Basically, each map has either a horizontal or vertiical mirror line where the two sides mirror
+Basically, each map has either a horizontal or vertical mirror line where the two sides mirror
 each other."#..#..#.." would have a mirror here: ".#..#.|.#...##". We see the mirror line, but not
 all indexes are mirrored. So what we need to do is not only to find the mirror line, but also how
 many rows/columns on each side actually mirror each other.
@@ -51,17 +51,136 @@ appear to mean is that for a mirror line to be valid it not only needs two ident
 next to each other, but also ALL surrounding rows/columns needs to have a mirror version at the
 same offset from the found mirror line. Any row/column whose mirror would be outside of the map
 can be ignored.
-
-This is the way.
 */
 mod part_one {
     use crate::reader;
     use std::error::Error;
 
-    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
-        let lines = reader::get_lines(data_path)?;
+    fn to_int(bits: [u8; 17]) -> u32 {
+        let mut result = 0;
+        for (i, bit) in bits.iter().enumerate() {
+            result += (*bit as u32) << i;
+        }
+        result
+    }
 
-        Err("NotImplemented: This problem has not been solved yet!".into())
+    fn remove_empty(list: &mut Vec<u32>) {
+        while let Some(last_value) = list.last() {
+            if *last_value == 0 {
+                list.remove(list.len() - 1);
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn into_values(grid: [[u8; 17]; 17]) -> (Vec<u32>, Vec<u32>) {
+        let mut rows = Vec::new();
+        for row in grid {
+            rows.push(to_int(row));
+        }
+        remove_empty(&mut rows);
+
+        let mut columns = Vec::new();
+        for x in 0..17 {
+            let mut column = [0; 17];
+            for (y, row) in grid.iter().enumerate() {
+                column[y] = row[x];
+            }
+            columns.push(to_int(column));
+        }
+        remove_empty(&mut columns);
+
+        (rows, columns)
+    }
+
+    fn is_mirrored(values: &[u32]) -> bool {
+        if values[0] == values[values.len() - 1] {
+            if values.len() == 2 {
+                return true;
+            }
+            return is_mirrored(&values[1..values.len() - 1]);
+        }
+        false
+    }
+
+    fn try_get_mirror_line_index(values: Vec<u32>) -> Option<u64> {
+        let len = values.len();
+
+        for i in (1..len).rev() {
+            if values[0] == values[i] && i % 2 == 1 && is_mirrored(&values[0..i + 1]) {
+                return Some((i + 1) as u64 / 2);
+            }
+        }
+
+        for i in 0..len - 1 {
+            if values[len - 1] == values[i] && (len - i) % 2 == 0 && is_mirrored(&values[i..len]) {
+                return Some((len - ((len - i) / 2)) as u64);
+            }
+        }
+
+        None
+    }
+
+    fn calculate_mirror_line_score(
+        rows: Vec<u32>,
+        columns: Vec<u32>,
+    ) -> Result<u64, Box<dyn Error>> {
+        Ok(
+            match (
+                try_get_mirror_line_index(rows),
+                try_get_mirror_line_index(columns),
+            ) {
+                (Some(value), None) => value * 100,
+                (None, Some(value)) => value,
+                (Some(value), Some(value2)) => {
+                    return Err(
+                        format!("Found two lines! row {} and column {}", value, value2).into(),
+                    )
+                }
+                _ => return Err("Couldn't find any mirror lines!".into()),
+            },
+        )
+    }
+
+    fn process_next(
+        data_lines: &mut impl Iterator<Item = String>,
+    ) -> Result<Option<u64>, Box<dyn Error>> {
+        let mut grid = [[0; 17]; 17];
+
+        let mut empty = true;
+
+        for (y, line) in data_lines.enumerate() {
+            empty = false;
+            if line.is_empty() {
+                break;
+            }
+            for (x, c) in line.chars().enumerate() {
+                grid[y][x] = match c {
+                    '#' => 1,
+                    '.' => 0,
+                    _ => return Err(format!("Found unexpected char [{c}] in data file!").into()),
+                }
+            }
+        }
+
+        if empty {
+            return Ok(None);
+        }
+
+        let (rows, columns) = into_values(grid);
+        Ok(Some(calculate_mirror_line_score(rows, columns)?))
+    }
+
+    pub fn calculate(data_path: &str) -> Result<u64, Box<dyn Error>> {
+        let mut lines = reader::get_lines(data_path)?;
+        let mut result = 0;
+
+        while let Some(score) = process_next(&mut lines)? {
+            result += score;
+        }
+
+        Ok(result)
     }
 }
 
